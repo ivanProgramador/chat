a6794f3f53a94ee09bedecea4c5126fb5cbb87d0
ivan <ivan-guita@hotmail.com>
2024-05-08 21:47:20 -0300

    Base da interface grafica criada

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+/*
+com socket.io não e possivel usar o servidor do express 
+precisa ser o servidor nativo de node por isso a blioteca http
+esta criando o serviddor ao inves da express
+*/
+var http = require("http").createServer(app); 
+var io = require("socket.io")(http);
+
+
+app.set("view engine","ejs");
+
+            app.get("/",(req,res)=>{
+                res.render("index");
+            });
+
+
+
+
+io.on("connection", (socket)=>{
+
+    
+    socket.on("disconnect",()=>{
+
+        console.log('Se desconectou: '+ socket.id);
+
+    });
+
+
+    
+});
+
+
+
+
+http.listen(3000,()=>{
+    console.log("App online");
+})
